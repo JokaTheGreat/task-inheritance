82f31d60ce51551b0588a3c689b0c80ff79735dd
Nikita Latysh <n.latysh@g.nsu.ru>
2022-04-08 19:16:09 +0700

    задание по наследованию Typescript

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -16,6 +16,20 @@ export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    constructor(personData: IPersonData) {
+        this._name = personData.name;
+        this._secondName = personData.secondName;
+        this._age = personData.age;
+    }
+
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
 
 /*
@@ -24,4 +38,20 @@ export class Person {
  */
 export class Student extends Person {
     readonly _phone: string;
+
+    constructor(studentData: IStudentData) {
+        super(studentData);
+        this._phone = studentData.phone;
+    }
+
+    getData(): IStudentData {
+        const personData = super.getData();
+
+        return {
+            name: personData.name,
+            secondName: personData.secondName,
+            age: personData.age,
+            phone: this._phone,
+        };
+    }
 }
